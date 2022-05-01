c6195a7473db568049a33e85a9725f79c7a9b595
simzim <info@simzim.lt>
2022-05-01 15:26:22 +0300

    login + logout + navigation

---
+++ src/main/java/lt/simzim/sportsclub/configuration/SecurityConfiguration.java
@@ -0,0 +1,72 @@
+package lt.simzim.sportsclub.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import lt.simzim.sportsclub.services.UserService;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
+
+	@Autowired
+	UserService userService;
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		
+		BCryptPasswordEncoder bc=new BCryptPasswordEncoder();	
+		
+		auth
+			.inMemoryAuthentication()
+				.withUser("admin").password(bc.encode("123")).roles("admin", "user")
+				.and()
+				.withUser("user").password(bc.encode("12345")).roles("user");		
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		
+		http
+			.authorizeRequests()
+				.antMatchers("/").permitAll()
+				.antMatchers("/client/new").hasAnyRole("admin")
+				.antMatchers("/workout/new").hasAnyRole("admin")
+				.antMatchers("/client/").permitAll()
+				.antMatchers("/workout/").permitAll()
+				
+				.antMatchers("/login*").permitAll()
+				.antMatchers("/regicter*").permitAll()
+//				.anyRequest().authenticated();
+		
+		.and()
+		.formLogin()
+			.loginPage("/login")
+			.defaultSuccessUrl("/")
+			.failureUrl("/login-error")
+	.and()
+		.logout()
+			.logoutUrl("/logout")
+			.logoutSuccessUrl("/");
+				
+	}
+
+
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	
+	
+
+}
---
+++ src/main/java/lt/simzim/sportsclub/services/UserService.java
@@ -0,0 +1,8 @@
+package lt.simzim.sportsclub.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+}
